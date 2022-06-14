896d730f2f101f4723debc9c775f9c2f9c10c2a0
Deiby Dayans R <dramoslance@gmail.com>
2022-06-14 14:00:02 -0400

    chore: add event factory

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            "banner" => $this->faker->imageUrl(),
+            "start_time" => $this->faker->dateTime(),
+            "end_time" => $this->faker->dateTime(),
+            "highlighted" => $this->faker->boolean(),
+            "timezone" => $this->faker->timezone(),
+            "status" => $this->faker->randomElement([0, 1])
+        ];
+    }
+}
