e0f51dedb44cbd97a73952e4dfc2b8f4a2f53ff3
vargas991 <edderson.vargas@unet.edu.ve>
2022-06-14 02:31:16 -0500

    feat: add speakers languages table migration

---
+++ database/migrations/2022_06_14_071256_create_speakers_languages_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('speakers_languages', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('name')->comment('The name of the translated speaker');
+            $table->string('biography')->comment('Brief biography of the speaker');
+            $table->unsignedBigInteger('language_id')->comment('The language identifier that represents the translation');
+            $table->unsignedBigInteger('speaker_id')->comment('The identifier of the speaker that is translated');
+            $table->unsignedBigInteger('created_user_id')->comment('This is the identifier of the translation user (it has no relationship, it works as a biting)')->nullable();
+            $table->unsignedBigInteger('modified_user_id')->comment('This is the identifier of the translation user (it has no relationship, it works as a biting)')->nullable();
+
+            $table
+                ->foreign('language_id')
+                ->references('id')
+                ->on('languages');
+            
+            $table
+                ->foreign('speaker_id')
+                ->references('id')
+                ->on('speakers');
+
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('speakers_languages');
+    }
+};
