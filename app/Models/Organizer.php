12cee51964b28c3e391480cea35399fc4bd1f4ca
Deiby Dayans R <dramoslance@gmail.com>
2022-06-14 13:56:26 -0400

    chore: add organizer model

---
+++ app/Models/Organizer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Organizer extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $table = 'organizers';
+
+
+    public function events()
+    {
+        return $this->hasMany(Event::class, 'organizer_id', 'id');
+    }
+
+
+    public function locations()
+    {
+        return $this->hasMany(Location::class, 'organizer_id', 'id');
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
