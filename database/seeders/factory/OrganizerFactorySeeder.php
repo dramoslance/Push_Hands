26e8fbd0f6a57fb1353224b6792aa1577c7d265f
vargas991 <edderson.vargas@unet.edu.ve>
2022-06-16 17:13:01 -0500

    feat: implement factory seeder of organizers languages table

--- database/seeders/factory/OrganizerFactorySeeder.php
+++ database/seeders/factory/OrganizerFactorySeeder.php
@@ -4,10 +4,13 @@ namespace Database\Seeders\Factory;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 use App\Models\User;
 use App\Models\Organizer;
+use App\Models\Language;
+
+use Faker\Factory as Faker;
 
 class OrganizerFactorySeeder extends Seeder
 {
@@ -28,10 +31,18 @@ class OrganizerFactorySeeder extends Seeder
         // Create organizers for with system users
         $organizers = Organizer::factory(10)
             ->create();
-
+        
+        $faker = Faker::create();
+    
+        $language= Language::factory()->create();
         foreach($organizers as $organizer) {
             $organizer->user()->associate(User::factory()->create());
             $organizer->save();
+
+            $organizer->languages()->attach($language, [
+                'name' => $faker->name(),
+                'description' => $faker->text($maxNbChars = 200),
+            ]);
         }
           
     }
