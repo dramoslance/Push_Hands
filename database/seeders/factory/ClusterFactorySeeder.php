4650251305f08bcce94c7e1522c4f3e18f007bb2
vargas991 <edderson.vargas@unet.edu.ve>
2022-06-16 17:12:04 -0500

    feat: implement factory seeder of clusters languages table

--- database/seeders/factory/ClusterFactorySeeder.php
+++ database/seeders/factory/ClusterFactorySeeder.php
@@ -4,9 +4,13 @@ namespace Database\Seeders\Factory;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 
 use App\Models\Cluster;
+use App\Models\Language;
+
+use Faker\Factory as Faker;
 
 class ClusterFactorySeeder extends Seeder
 {
@@ -19,8 +23,20 @@ class ClusterFactorySeeder extends Seeder
     {
         //
 
-        Cluster::factory(10)
+        $clusters = Cluster::factory(10)
             ->create();
+
+        $faker = Faker::create();
+        
+        $language= Language::factory()->create();
+
+        foreach($clusters as $cluster){
+            $cluster->languages()->attach($language, [
+                'name' => $faker->name(),
+                'slug' => $faker->slug(),
+                'description' => $faker->text($maxNbChars = 200)
+            ]);
+        }
 
     }
 }
