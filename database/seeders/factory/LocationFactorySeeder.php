bfedce89d6ddd915e686ebeb7b279b5f473778e1
vargas991 <edderson.vargas@unet.edu.ve>
2022-06-16 17:12:45 -0500

    feat: implement factory seeder of locations languages table

--- database/seeders/factory/LocationFactorySeeder.php
+++ database/seeders/factory/LocationFactorySeeder.php
@@ -4,10 +4,14 @@ namespace Database\Seeders\Factory;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 use App\Models\Location;
 use App\Models\Organizer;
+use App\Models\Language;
+
+use Faker\Factory as Faker;
+
 
 class LocationFactorySeeder extends Seeder
 {
@@ -19,19 +23,29 @@ class LocationFactorySeeder extends Seeder
     public function run()
     {
         //
+        $faker = Faker::create();
 
         // Create locations for the same organizer
-        Location::factory(10)
+        $locations = Location::factory(10)
             ->for(Organizer::factory()->create())
             ->create();
+
+        $language= Language::factory()->create();
+        foreach($locations as $location){
+            $location->languages()->attach($language, [
+                'name' => $faker->catchPhrase(),
+                'description' => $faker->text($maxNbChars = 20),
+                'adrees_line' => $faker->address()
+            ]);
+        }
 
         // Create locations for many organizers
 
-        
         foreach(Organizer::factory(10)->create() as $organizer) {
-             Location::factory(1)
-            ->for(Organizer::factory()->create())
-            ->create();
+           Location::factory(1)
+                ->for(Organizer::factory()->create())
+                ->create();
+
         }
 
     }
