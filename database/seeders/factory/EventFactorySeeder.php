d9c62170ad0f47aa3c774746fd57e49865706db3
vargas991 <edderson.vargas@unet.edu.ve>
2022-06-16 17:12:26 -0500

    feat: implement factory seeder of events languages table

--- database/seeders/factory/EventFactorySeeder.php
+++ database/seeders/factory/EventFactorySeeder.php
@@ -4,11 +4,17 @@ namespace Database\Seeders\Factory;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 use App\Models\Event;
 use App\Models\Organizer;
 use App\Models\Location;
 use App\Models\Cluster;
+use App\Models\Language;
+
+
+use Faker\Factory as Faker;
+
 
 class EventFactorySeeder extends Seeder
 {
@@ -23,7 +29,7 @@ class EventFactorySeeder extends Seeder
 
         $organizer = Organizer::factory()->create();
 
-        Event::factory(10)
+        $events = Event::factory(10)
             ->for($organizer)
             ->for(
                 Location::factory()
@@ -32,5 +38,18 @@ class EventFactorySeeder extends Seeder
             )
             ->for(Cluster::factory()->create())
             ->create();
+
+        $faker = Faker::create();
+        
+        $language= Language::factory()->create();
+
+        foreach($events as $event){
+            $event->languages()->attach($language, [
+                'name' => $faker->catchPhrase(),
+                'short_name' => $faker->text($maxNbChars = 20),
+                'tagline' => $faker->jobTitle(),
+                'slug' => $faker->slug()
+            ]);
+        }
     }
 }
