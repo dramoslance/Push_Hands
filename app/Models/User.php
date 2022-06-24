9f2a7c22aae29d480f74f627c34d01b09f8d4add
Gian vespa <givespas@gmail.com>
2022-06-24 11:21:55 -0500

    feat: use spatie in model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -43,7 +44,8 @@ class User extends Authenticatable
     ];
 
 
-    public function organizer(){
+    public function organizer()
+    {
         return $this->hasOne(Organizer::class, 'user_id', 'id');
     }
 
