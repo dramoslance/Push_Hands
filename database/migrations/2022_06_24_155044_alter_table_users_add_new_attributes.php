94a7be221ba4893798e569fb1e356662dd5d4e8a
Gian vespa <givespas@gmail.com>
2022-06-24 13:27:26 -0500

    feat: alter table users add attributes

---
+++ database/migrations/2022_06_24_155044_alter_table_users_add_new_attributes.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('password');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->string('lastname')
+                ->after('name')
+                ->comment('This is the users last name');
+
+            $table->string('username')
+                ->after('lastname')
+                ->unique()
+                ->comment('This is the username of the user')
+                ->nullable();
+
+            $table->time('birth_date')
+                ->after('username')
+                ->comment('This is the users date of birth');
+
+            $table->string('portrait')
+                ->after('username')
+                ->comment('This is the users cover photo');
+
+            $table->string('password')
+                ->after('email')
+                ->nullable();
+
+            $table->unsignedBigInteger('created_user_id')
+                ->comment('This is the identifier of the user who creates the translation(it has no relationship, it works as a bitacora)')
+                ->nullable();
+
+            $table->unsignedBigInteger('modified_user_id')
+                ->comment('This is the identifier of the user who modifies the translation (it has no relationship, it works as a bitacora)')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('users', 'lastname')) {
+            Schema::table('users', function (Blueprint $table) {
+
+                $table->dropColumn(
+                    [
+                        'lastname',
+                        'username',
+                        'birth_date',
+                        'portrait',
+                        'password',
+                        'created_user_id',
+                        'modified_user_id'
+                    ]
+                );
+            });
+
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('password')
+                    ->after('email_verified_at');
+            });
+        }
+    }
+};
