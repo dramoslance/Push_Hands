7fd86d24e8036d8c8f3b1553ca326510d1e497c3
vargas991 <edderson.vargas@unet.edu.ve>
2022-07-12 02:27:13 -0500

    chore: create routes of organizer

---
+++ routes/api/organizer.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\OrganizerController;
+
+Route::middleware('auth:api')->get('/', [OrganizerController::class, 'index'])->name('organizer.index');
+Route::post('/',[OrganizerController::class, 'store'])->name('organizer.store');
+Route::put('/',[OrganizerController::class, 'update'])->name('organizer.update');
+Route::get('/{id}', [OrganizerController::class, 'show'])->name('organizer.show');
+Route::delete('/{id}', [OrganizerController::class, 'destroy'])->name('organizer.destroy');
