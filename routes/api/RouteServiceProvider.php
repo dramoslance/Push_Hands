fb3ceae374587f65b54ec57a65b05742cacc1974
vargas991 <edderson.vargas@unet.edu.ve>
2022-07-12 02:30:07 -0500

    refactor: add file of orginizer routes to providers route

--- routes/api/RouteServiceProvider.php
+++ routes/api/RouteServiceProvider.php
@@ -7,3 +7,6 @@ Route::middleware('api')->prefix('auth')->group(base_path('routes/api/auth.php')
 
 // routes event
 Route::middleware('api')->prefix('event')->group(base_path('routes/api/event.php'));
+
+// routes organizer
+Route::middleware('api')->prefix('organizer')->group(base_path('routes/api/organizer.php'));
