5e1e98f3a237eac8cb4250e1422ea7e54b57eca2
vargas991 <edderson.vargas@unet.edu.ve>
2022-07-12 02:28:16 -0500

    chore: create form request of organizer controller

---
+++ app/Http/Requests/Organizer/StoreOrganizerRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Organizer;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrganizerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'portrait' => 'string|max:255',
+            'email' => 'string|required|email|unique:organizers',
+            'phone' => 'string|required|unique:organizers',
+            'website' => 'string'
+        ];
+    }
+}
