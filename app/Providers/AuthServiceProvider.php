63b5f7a0a2ccbc1b1773d891e871a19439c073bb
Gian vespa <givespas@gmail.com>
2022-07-14 14:46:46 -0500

    feat: give all permissions to super admin role

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Gate;
+use interface\RolePermission\RolesEntity;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -30,5 +32,9 @@ class AuthServiceProvider extends ServiceProvider
         // Passport::tokensExpireIn(now()->addDays(15));
         // Passport::refreshTokensExpireIn(now()->addDays(30));
         // Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole(RolesEntity::SUPER_ADMIN) ? true : null;
+        });
     }
 }
