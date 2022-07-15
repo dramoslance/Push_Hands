d322e3f0834c35a5d953bf5da719ae0b97c5e290
vargas991 <edderson.vargas@unet.edu.ve>
2022-07-14 23:04:26 -0500

    refactor: edit route get show to return a organizer and you respective data on organizer language table

--- routes/api/organizer.php
+++ routes/api/organizer.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\Api\OrganizerController;
 Route::middleware('auth:api')->get('/', [OrganizerController::class, 'index'])->name('organizer.index');
 Route::post('/',[OrganizerController::class, 'store'])->name('organizer.store');
 Route::put('/',[OrganizerController::class, 'update'])->name('organizer.update');
-Route::get('/{id}', [OrganizerController::class, 'show'])->name('organizer.show');
+Route::get('/{language_id}/{id}', [OrganizerController::class, 'show'])->name('organizer.show');
 Route::delete('/{id}', [OrganizerController::class, 'destroy'])->name('organizer.destroy');
