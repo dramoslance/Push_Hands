8a43ae2368938f89bed3f876b7dcaab173369e56
Gian vespa <givespas@gmail.com>
2022-07-16 04:13:35 -0500

    feat: added routes (verify, pass reset, pass forgot)

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -4,17 +4,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\ResetPasswordController;
+use App\Http\Controllers\Api\Auth\ForgotPasswordController;
+use App\Http\Controllers\Api\Auth\VerifyEmailController;
 use App\Http\Controllers\Api\Auth\RoleController;
 use App\Http\Controllers\Api\Auth\PermissionController;
 
 Route::post('/register', [RegisterController::class, 'store'])->name('auth.register');
 Route::post('/login', [LoginController::class, 'store'])->name('auth.login');
+Route::delete('/logout', [LogoutController::class, 'delete'])->middleware('auth:api')->name('auth.logout');
 
-Route::group(['middleware' => ['auth:api']], function () {
-    Route::delete('/logout', [LogoutController::class, 'delete'])->name('auth.logout');
-    Route::get('/role', [RoleController::class, 'index'])->name('auth.get.role');
-    Route::get('/permission', [PermissionController::class, 'index'])->name('auth.get.permission');
-});
+Route::put('/password/reset', [ResetPasswordController::class, 'update'])->middleware('auth:api')->name('auth.change.password');
+Route::post('/password/forgot', [ForgotPasswordController::class, 'send'])->name('auth.send.forgot.password');
+Route::patch('/password/forgot/{token}', [ForgotPasswordController::class, 'update'])->name('auth.change.forgot.password');
+
+Route::get('/verify', [VerifyEmailController::class, 'send'])->middleware('auth:api')->name('auth.send.verify');
+Route::get('/verify/{token}', [VerifyEmailController::class, 'verify'])->name('auth.get.verify');
+
+Route::get('/role', [RoleController::class, 'index'])->middleware('auth:api')->name('auth.get.role');
+Route::get('/permission', [PermissionController::class, 'index'])->middleware('auth:api')->name('auth.get.permission');
 
 Route::group(['middleware' => ['role:super_admin|admin', 'auth:api']], function () {
     Route::post('/role', [RoleController::class, 'store'])->name('auth.set.role');
