e1180a7fe8ac672c532fb64e25eaeb17dc8e3c6e
Gian vespa <givespas@gmail.com>
2022-07-21 22:35:23 -0500

    feat: new func getLanguage in apiController

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Language;
 
 class ApiController extends Controller
 {
@@ -28,5 +29,11 @@ class ApiController extends Controller
         }
 
         return response()->json($response, $code);
+    }
+
+    public function getLanguageModel(string $iso_code = null): Language
+    {
+        if (empty($iso_code)) $iso_code =  env('DEFAULT_LANGUAGE');
+        return Language::where(['iso_code' => $iso_code])->first();
     }
 }
