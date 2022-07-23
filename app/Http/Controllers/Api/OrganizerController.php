39f7d9a2eecca87565bd16a1f6204d07e9488aba
Gian vespa <givespas@gmail.com>
2022-07-23 17:26:39 -0500

    refactor: OrganizerController in method (index, show)

--- app/Http/Controllers/Api/OrganizerController.php
+++ app/Http/Controllers/Api/OrganizerController.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Organizer\StoreTranslationOrganizerRequest;
 use App\Http\Requests\Organizer\StoreOrganizerRequest;
 use App\Http\Requests\Organizer\UpdateOrganizerRequest;
-use App\Http\Requests\Organizer\StoreTranslationOrganizerRequest;
-use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\LangRequest;
 use App\Models\Organizer;
-use App\Models\Language;
-use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
 
 class OrganizerController extends ApiController
 {
@@ -18,43 +15,28 @@ class OrganizerController extends ApiController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index(LangRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'lang_code' => 'string|exists:App\Models\Language,iso_code',
-        ]);
+        $organizers = Organizer::multiLanguages();
 
-        if ($validator->fails()) {
-            return $this->sendError('Validation Error.', $validator->errors());
+        return $this->sendResponse($organizers, 'ok');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id, LangRequest $request)
+    {
+        if (empty($request->lang_code)) {
+            $organizer = Organizer::multiLanguages($id);
+        } else {
+            $organizer = Organizer::language($id);
         }
 
-        $language = $this->getLanguageModel($request->input('lang_code'));
-
-        $organizers = Organizer::all();
-
-        $orga = [];
-
-        foreach ($organizers as $key => $organizer) {
-            $organizer_language = Organizer::find($organizer->id)
-                ->languages()
-                ->where('language_id', $language->id)
-                ->get();
-           
-            if (count($organizer_language) > 0) {
-                return $organizer_language;
-                $orga['organizers'][] = [
-                    'id' => $organizer->id,
-                    'portrait' => $organizer->portrait,
-                    'name'=> $organizer_language[0]->pivot->name,
-                    'description'=> $organizer_language[0]->pivot->description,
-                    'email' => $organizer->email,
-                    'phone' => $organizer->phone,
-                    'website' => $organizer->website
-                ];
-            } 
-        }
-
-        return $this->sendResponse($orga, 'User register successfully.');
+        return $this->sendResponse($organizer, 'ok');
     }
 
     /**
@@ -65,13 +47,6 @@ class OrganizerController extends ApiController
      */
     public function store(StoreOrganizerRequest $request)
     {
-        // $iso_code = $request->input('iso_code') ?  $request->input('iso_code') : 'ro_MD';
-
-        // $language = Language::where([
-        //     'iso_code' => $iso_code
-        // ])
-        //     ->first();
-
         $language = $this->getLanguageModel($request->input('lang_code'));
 
         $organizer = Organizer::create([
@@ -111,51 +86,7 @@ class OrganizerController extends ApiController
         return $this->sendResponse($organizer, 'Translation of organization created successfully');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id, Request $request)
-    {
-
-        //todo: sustituir por el valor por defecto, dependiendo del enfoque definido, variable de entorno, o idioma preferido de usuario
-
-        $iso_code = $request->input('lang_code') ?  $request->input('lang_code') : 'ro_MD';
-        $organizer = Organizer::where([
-            'id' => $id
-        ])
-            ->with(['languages' => function ($query) use ($iso_code) {
-                $query->where('iso_code', $iso_code)->first();
-            }])
-            ->first();
-
-        if (count($organizer->languages) === 0)
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Organizer doesn\'t not have translation in that language.'
-            ], 422);
-
-        $organizer_json = [
-            'id' => $organizer->id,
-            'name' => $organizer->languages[0]->pivot->name,
-            'description' => $organizer->languages[0]->pivot->description,
-            'portrait' => $organizer->portrait,
-            'email' => $organizer->email,
-            'phone' => $organizer->phone,
-            'website' => $organizer->website
-        ];
-
-        return response()->json([
-            'status' => 'ok',
-            'data' => response()->json([
-                'organizer' => $organizer_json,
-            ])
-        ], 200);
-    }
-
-    /**
+    /**Asi 
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -164,12 +95,11 @@ class OrganizerController extends ApiController
      */
     public function update(UpdateOrganizerRequest $request)
     {
-
         $iso_code = $request->input('iso_code') ?  $request->input('iso_code') : 'ro_MD';
 
         $organizer = Organizer::where([
             'id' => $request->input('organizer_id')
-            ])
+        ])
             ->with(['languages' => function ($query) use ($iso_code) {
                 $query->where('iso_code', $iso_code)->first();
             }])
