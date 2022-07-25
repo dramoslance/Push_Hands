78e1a644cf9610e52358dbde54eff8dcd9865d72
Gian vespa <givespas@gmail.com>
2022-07-25 17:44:35 -0500

    feat: assign member method store, update to modify

---
+++ app/Http/Controllers/Api/Organizer/MemberController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api\Organizer;
+
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Requests\Organizer\StoreMemberOrganizerRequest;
+use App\Models\Organizer;
+use App\Models\User;
+
+class MemberController extends ApiController
+{
+    /**
+     * Store a newly traduction created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMemberOrganizerRequest $request)
+    {
+        $organizer = Organizer::find($request->input('organizer_id'));
+
+        $user = $this->getUser();
+        if ($user->id !== $request->input('organizer_user_id')) {
+            $user = User::find($request->input('organizer_user_id'));
+        }
+
+        $organizer->members()->attach($user, [
+            'created_user_id' => $user->id,
+            'modified_user_id' => $user->id
+        ]);
+
+        return $this->sendResponse($organizer, 'Member of organizer successfully');
+    }
+
+    public function update()
+    {
+    }
+}
