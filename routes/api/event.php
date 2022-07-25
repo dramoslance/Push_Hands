4adb2ce474abfbb7694ef05de37a50b35e88a6dc
Gian vespa <givespas@gmail.com>
2022-07-25 15:18:22 -0500

    feat: group middleware route (event, organizer)

--- routes/api/event.php
+++ routes/api/event.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventController;
 
-Route::middleware('auth:api')->get('/', [EventController::class, 'index'])->name('event.index');
-Route::get('/{id}', [EventController::class, 'show'])->name('event.show');
-Route::delete('/{id}', [EventController::class, 'destroy'])->name('event.destroy');
+Route::group(['middleware' => ['role:admin|user', 'auth:api']], function () {
+    Route::get('/', [EventController::class, 'index'])->name('event.index');
+    Route::get('/{id}', [EventController::class, 'show'])->name('event.show');
+    Route::delete('/{id}', [EventController::class, 'destroy'])->name('event.destroy');
+});
--- routes/api/organizer.php
+++ routes/api/organizer.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\OrganizerController;
 
-Route::middleware('auth:api')->get('/', [OrganizerController::class, 'index'])->name('organizer.index');
-Route::post('/',[OrganizerController::class, 'store'])->name('organizer.store');
-Route::post('/translation',[OrganizerController::class, 'storeOrganizerTranslation']);
-Route::put('/',[OrganizerController::class, 'update'])->name('organizer.update');
-Route::get('/{id}', [OrganizerController::class, 'show'])->name('organizer.show');
-Route::delete('/{id}', [OrganizerController::class, 'destroy'])->name('organizer.destroy');
+Route::group(['middleware' => ['role:admin', 'auth:api']], function () {
+    Route::get('/', [OrganizerController::class, 'index'])->name('organizer.index');
+    Route::post('/', [OrganizerController::class, 'store'])->name('organizer.store');
+    Route::post('/translation', [OrganizerController::class, 'storeTranslation'])->name('organizer.translation.store');
+    Route::put('/', [OrganizerController::class, 'update'])->name('organizer.update');
+    Route::get('/{id}', [OrganizerController::class, 'show'])->name('organizer.show');
+    Route::delete('/{id}', [OrganizerController::class, 'destroy'])->name('organizer.destroy');
+});
