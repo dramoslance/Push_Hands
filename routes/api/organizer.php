17b119e4045462b0b5ae91b432d27637d9de3b04
Gian vespa <givespas@gmail.com>
2022-07-25 17:46:44 -0500

    fix: router organizer

--- routes/api/organizer.php
+++ routes/api/organizer.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\OrganizerController;
+use App\Http\Controllers\Api\Organizer\ShowController;
+use App\Http\Controllers\Api\Organizer\StoreController;
+use App\Http\Controllers\Api\Organizer\UpdateController;
+use App\Http\Controllers\Api\Organizer\DestroyController;
+use App\Http\Controllers\Api\Organizer\TranslationController;
+use App\Http\Controllers\Api\Organizer\MemberController;
 
 Route::group(['middleware' => ['role:admin', 'auth:api']], function () {
-    Route::get('/', [OrganizerController::class, 'index'])->name('organizer.index');
-    Route::post('/', [OrganizerController::class, 'store'])->name('organizer.store');
-    Route::post('/translation', [OrganizerController::class, 'storeTranslation'])->name('organizer.translation.store');
-    Route::put('/', [OrganizerController::class, 'update'])->name('organizer.update');
-    Route::get('/{id}', [OrganizerController::class, 'show'])->name('organizer.show');
-    Route::delete('/{id}', [OrganizerController::class, 'destroy'])->name('organizer.destroy');
+    Route::get('/', [ShowController::class, 'index'])->name('organizer.index');
+    Route::get('/{id}', [ShowController::class, 'show'])->name('organizer.show');
+
+    Route::post('/', [StoreController::class, 'store'])->name('organizer.store');
+    Route::put('/', [UpdateController::class, 'update'])->name('organizer.update');
+    Route::delete('/{id}', [DestroyController::class, 'destroy'])->name('organizer.destroy');
+
+    Route::post('/translation', [TranslationController::class, 'store'])->name('organizer.translation.store');
+    Route::post('/member', [MemberController::class, 'store'])->name('organizer.member.store');
 });
